6b96a64835b5ff4d8e35a11dd09496ca6f0771c8
anjanStha <anjanstha61@gmail.com>
2025-03-18 19:57:31 +1100

    update triangle code

--- Assignment_S1_1/A1_Triangle.py
+++ Assignment_S1_1/A1_Triangle.py
@@ -1,22 +1,50 @@
-def check_valid_number(count):
-    while True:
-        try:
-            num = int(input(f'Enter input {count}: '))
-            return num
-        except ValueError:
-            print('Invalid input. Please enter a valid number.')
-            continue
+
+##a. Ask the user to input three numbers, and check if these three numbers can form a triangle. 
+    ##  Need three inputs. 
+    ## T he output shows whether the result can form a triangle or not.
+
+# check for triangle 
+def check_triangle(a, b, c):
+   return a + b > c and a + c > b and b + c > a
+
+# Function to process user input for check int, float , char
+def check_input(user_input):
+    try:
+
+        if user_input.isdigit():
+            return int(user_input)
+        
+   
+     # Try converting to float
+        float_number = float(user_input)
+        int_number = round(float_number, 0)
+        print(f"User input number {user_input} is converted to integer number {int_number}")
+        return int_number
+    
+    except ValueError:
+         # If input value is  character
+        ascii_value = [ord(char) for char in user_input]
+        print(f"User input character  {user_input} is converted to ascii value {ascii_value}")
+        int_number = sum(ascii_value)
+        if type(int_number) == int:
+            return int_number
+        else: 
+            return 0
+           
 
 
-def check_triangle():
-    tri_sides =  [check_valid_number(count) for count in range(1, 4)]
-    max_side = max(tri_sides)
-    tri_sides.remove(max_side)
-    sum_sides = sum(tri_sides)
+# Taking user inputs
+a = check_input(input("Please enter first side of Triangle : "))
+b = check_input(input("Please enter second side of Triangle : "))
+c = check_input(input("Please enter Third side of Triangle : "))
+print(f"a = {a} ")
+print(f"b = {b} ")
+print(f"c = {c} ")
 
-    if sum_sides > max_side:
-        return 'Yes, these three lengths can form a triangle.'
-    else:
-        return 'No, these three lengths cannot form a triangle.'
 
-print(check_triangle())
+#### lets check the result
+if check_triangle(a, b, c):
+    print("Yes, these three lengths can form a triangle. ") 
+else:
+    print("'No, these three lengths cannot form a triangle. ") 
+
